919e81d0f4a8dd80d0241f7b4de70b86209ec3e7
min0908 <94737255+min0908@users.noreply.github.com>
2023-02-27 23:49:37 +0900

    Create The seaborn objects inferface.py

---
+++ seaborn_study/The seaborn objects inferface.py
@@ -0,0 +1,44 @@
+# Specifying a plot and mapping data
+import seaborn.objects as so
+
+so.Plot(penguins, x="bill_length_mm", y="bill_depth_mm").add(so.Dot())
+
+so.Plot(penguins, x="bill_length_mm", y="bill_depth_mm").add(so.Dot(color="g", pointsize=4))
+
+
+so.Plot(penguins, x="bill_length_mm", y="bill_depth_mm",
+        color="species", pointsize="body_mass_g").add(so.Dot())
+
+so.Plot(penguins, x="bill_length_mm", y="bill_depth_mm",
+        edgecolor="sex", edgewidth="body_mass_g").add(so.Dot(color=".8"))
+
+# Defining groups
+so.Plot(healthexp, x="Year", y="Life_Expectancy", color="Country").add(so.Line())
+
+so.Plot(healthexp, x="Year", y="Life_Expectancy", group="Country").add(so.Line())
+
+# Transforming data before plotting
+so.Plot(penguins, x="species", y="body_mass_g").add(so.Bar(), so.Agg())
+
+so.Plot(penguins, x="species", y="body_mass_g").add(so.Dot(pointsize=10), so.Agg())
+
+so.Plot(penguins, x="species", y="body_mass_g", color="sex").add(so.Dot(pointsize=10), so.Agg())
+
+# Resolving overplotting
+so.Plot(penguins, x="species", y="body_mass_g", color="sex").add(so.Bar(), so.Agg())
+
+so.Plot(penguins, x="species", y="body_mass_g", color="sex").add(so.Bar(), so.Agg(), so.Dodge())
+
+so.Plot(penguins, x="species", y="body_mass_g", color="sex").add(so.Dot(), so.Dodge())
+
+so.Plot(penguins, x="species", y="body_mass_g", color="sex").add(so.Dot(), so.Dodge(), so.Jitter(.3))
+
+
+# Creating variables through transformation
+so.Plot(penguins, x="species").add(so.Bar(), so.Hist())
+
+so.Plot(penguins, x="flipper_length_mm").add(so.Bars(), so.Hist())
+
+(so.Plot(penguins, x="body_mass_g", y="species", color="sex").add(so.Range(), so.Est(errorbar="sd"), so.Dodge())
+    .add(so.Dot(), so.Agg(), so.Dodge())
+)
